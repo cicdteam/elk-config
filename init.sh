dc45d954d912eeff99cf14106f5e282b0252e1c4
pureclouds <me@pureclouds.net>
2016-12-25 14:59:22 +0000

    fixes

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo -n "${ELK_USER}:" > /etc/nginx/conf.d/htpasswd.users
-openssl passwd -apr1 -noverify "${ELK_APSS}" >> /etc/nginx/conf.d/htpasswd.users
+openssl passwd -apr1 -noverify "${ELK_PASS}" >> /etc/nginx/conf.d/htpasswd.users
