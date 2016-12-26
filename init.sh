eee9c404b90b30144900f3489c0334e53fd50f2b
pureclouds <me@pureclouds.net>
2016-12-26 06:22:21 +0000

    memory configuration added

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+sysctl -w vm.max_map_count=262144
+
 echo -n "${ELK_USER}:" > /etc/nginx/conf.d/htpasswd.users
 openssl passwd -apr1 -noverify "${ELK_PASS}" >> /etc/nginx/conf.d/htpasswd.users
